6d7efa5939924c311680d39bb5c4337e8c647a0e
jonathan happy <jonathanlipro@gmail.com>
2023-07-22 13:57:52 +0200

    ajout de la table de y

--- src/main/java/cm/Jtech/Tabledemultiplication.java
+++ src/main/java/cm/Jtech/Tabledemultiplication.java
@@ -11,8 +11,9 @@ public class Tabledemultiplication {
         }*/
 
 
-        int x;
+        int x,y;
         x=7;
+        y=3;
         System.out.println("soit le nombre x = 9");
         System.out.println("Les multiples de 9 sont :");
         System.out.println(
@@ -26,5 +27,17 @@ public class Tabledemultiplication {
                 x+" x 7 ="+ x*7 +"\n" +
                 x+" x 8 ="+ x*8 +"\n" +
                 x+" x 9 ="+ x*9 +"\n" );
+
+        System.out.println(
+                y+" x 0 ="+ y*0 +"\n" +
+                y+" x 1 ="+ y*1 +"\n" +
+                y+" x 2 ="+ y*2 +"\n" +
+                y+" x 3 ="+ y*3 +"\n" +
+                y+" x 4 ="+ y*4 +"\n" +
+                y+" x 5 ="+ y*5 +"\n" +
+                y+" x 6 ="+ y*6 +"\n" +
+                y+" x 7 ="+ y*7 +"\n" +
+                y+" x 8 ="+ y*8 +"\n" +
+                y+" x 9 ="+ y*9 +"\n" );
     }
 }
